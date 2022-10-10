4fa5f0aa0f0a8284b3011ef6017a80ec980babbc
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-10-10 12:27:54 -0300

    Scripts Unity

---
+++ Devils Crown/Devil's Crown/Assets/Scripts/Shooting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;   
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

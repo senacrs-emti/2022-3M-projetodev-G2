a6d2d29c23c440de6f8141cdcc48b9e829812d42
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-10-17 11:12:44 -0300

    Unity dev

--- Devils Crown/Devil's Crown/Assets/Scripts/CameraFollow.cs
+++ Devils Crown/Devil's Crown/Assets/Scripts/CameraFollow.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
     public float FollowSpeed = 2f;
+    public float yOffset = 1f;
     public Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }

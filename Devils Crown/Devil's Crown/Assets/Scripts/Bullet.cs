645950541d1db4975f227f0c726f2ddf0bf5cd59
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-11-07 11:19:31 -0300

    Unity Dev

--- Devils Crown/Devil's Crown/Assets/Scripts/Bullet.cs
+++ Devils Crown/Devil's Crown/Assets/Scripts/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        transform.Translate(Vector3.up * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)

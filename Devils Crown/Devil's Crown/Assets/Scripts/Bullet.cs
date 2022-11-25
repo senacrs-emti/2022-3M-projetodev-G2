fbcce3b4f708c177a8467594c678dfb58202454a
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-11-25 10:40:16 -0300

    Scripts

--- Devils Crown/Devil's Crown/Assets/Scripts/Bullet.cs
+++ Devils Crown/Devil's Crown/Assets/Scripts/Bullet.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour{
 
     [SerializeField] float Speed;
-    
-    void Update(){
+    [SerializeField] float MaxDistance = 10;
+    [SerializeField] int Damage = 5;
+
+    private Rigidbody2D rb2d;
+    private float conquaredDistance = 0; 
+
+    void Update()
+    {
         transform.Translate(Vector3.right * Time.deltaTime * Speed);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Colliderd " + collision.name);
         Destroy(gameObject);
     }
 }
